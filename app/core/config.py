864d3c8c9a421742fb1bd1859246bbd170573ca5
enaa99 <wprkfdlsgk@gmail.com>
2025-01-21 23:43:16 +0900

    fix: remove default value to connect mysql

--- app/core/config.py
+++ app/core/config.py
@@ -3,10 +3,10 @@ import os
 
 class Settings:
     # MySQL DB 설정
-    MYSQL_DB_URL = os.getenv("MYSQL_DB_URL", "mysql+pymysql://username:password@localhost/db_name")
+    MYSQL_DB_URL = os.getenv("MYSQL_DB_URL", "")
     
     # MongoDB 설정
-    MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
-    MONGODB_DB_NAME = os.getenv("MONGODB_DB_NAME", "my_database")
+    MONGODB_URI = os.getenv("MONGODB_URI", "")
+    MONGODB_DB_NAME = os.getenv("MONGODB_DB_NAME", "")
 
 settings = Settings()
